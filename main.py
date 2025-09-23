279aaa929dd403a31f8541ffd37661987c9c4700
J.H <jiyue_he@163.com>
2025-09-23 15:53:19 -0700

    .

--- main.py
+++ main.py
@@ -37,5 +37,16 @@ fiber_flag = 0  # no fiber
 r = []  # no fiber
 fiber_orientation = []  # no fiber
 
+# pacing
 pacing_voxel_id = np.array([36184, 36190, 36191, 36198, 36693, 36694, 36695, 36699, 36700, 36701, 36705, 36706, 36707, 37187, 37192, 37193, 37194, 37199])
+
+# duration
+pacing_start_time = 1 # ms
+pacing_cycle_length = 180 # ms
+
+n = 10 # ms
+pacing_duration = n/dt # make sure it is n ms no matter what dt is
+
+pacing_starts = np.arange(pacing_start_time/dt, t_final/dt - pacing_duration + 1, pacing_cycle_length/dt)
+pacing_ends = pacing_starts + pacing_duration - 1
 
