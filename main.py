c985a137bd91298c265bb6c3f85ba3ab69dfd016
J.H <jiyue_he@163.com>
2025-09-26 02:08:14 -0700

    display activation movie in internet browser with interfaces

--- main.py
+++ main.py
@@ -214,29 +214,36 @@ if do_flag == 1:
         color = codes.convert_data_to_color.execute(data, data_min, data_max, data_threshold)
         map_color[n] = color
 
-    # display movie
+    # display movie (will open in internet browser)
     fig = go.Figure(
-        data=[go.Mesh3d(
-            x=vertex[:, 0], y=vertex[:, 1], z=vertex[:, 2],
-            i=face[:, 0], j=face[:, 1], k=face[:, 2],
-            vertexcolor=map_color[0]   # start with first frame
-        )],
+        data=[
+            go.Mesh3d(
+                x=vertex[:, 0], y=vertex[:, 1], z=vertex[:, 2],
+                i=face[:, 0], j=face[:, 1], k=face[:, 2],
+                vertexcolor=map_color[0])
+        ],
+
         layout=go.Layout(
             updatemenus=[{
                 "type": "buttons",
                 "buttons": [
-                    {"label": "Play", "method": "animate", "args": [None, {"frame": {"duration": 10}}]},
-                    {"label": "Pause", "method": "animate", "args": [[None], {"frame": {"duration": 0}, "mode": "immediate"}]}
+                    {"label": "Play", "method": "animate", "args": [None, {"frame": {"duration": 100}, "fromcurrent": True, "mode": "immediate"}]},
+                    {"label": "Pause", "method": "animate", "args": [[None], {"frame": {"duration": 0}, "mode": "immediate", "transition": {"duration": 0}}]}
                 ]
+            }],
+
+            sliders=[{
+                'active': 0,
+                'currentvalue': {"prefix": "Time: "},
+                'pad': {"t": 50},
+                'steps': [{'label': str(t), 'method': 'animate', 'args': [[str(t)], {'frame': {'duration': 0}, 'mode': 'immediate'}]}
+                for t in range(len(map_color))]
             }]
         ),
+
         frames=[
             go.Frame(data=[go.Mesh3d(vertexcolor=map_color[t])], name=str(t))
             for t in range(len(map_color))
         ]
     )
     fig.show()
-
-
-
-
